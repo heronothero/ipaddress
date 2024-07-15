d89d4250cc3b35f7403a0dbbbca4e53de4f2d135
heronothero <mazayadasha@gmail.com>
2024-07-16 04:37:38 +0700

    создание контроллеров

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Services\IPInfo\IPAPIService;
+
+class RegisterController extends Controller
+{
+    
+}
