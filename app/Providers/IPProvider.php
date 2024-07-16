3a6f1687101b511fcc48c443099b74d43801913e
heronothero <mazayadasha@gmail.com>
2024-07-16 20:30:06 +0700

    исправление возвратимого значения провайдера

--- app/Providers/IPProvider.php
+++ app/Providers/IPProvider.php
@@ -5,16 +5,15 @@ namespace App\Providers;
 
 use App\Services\IPInfo\IPAPIService;
 use App\Services\IPInfo\IPInfoContract;
-use App\Services\IPInfo\RateLimit;
-use App\Services\IPInfo\RateLimitContract;
 use Illuminate\Support\ServiceProvider;
+use App\Services\IPInfo\RateLimit;
 
 class IPProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->singleton(IPInfoContract::class, function () {
-            return new IPAPIService(RateLimit(40));
+            return new IPAPIService(new RateLimit(40));
         });
     }
     public function boot(): void
