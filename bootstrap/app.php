794c9169d1afb2ccff60c8b48f7e1c36a7585068
heronothero <mazayadasha@gmail.com>
2024-07-16 20:50:12 +0700

    редактирование

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
--- routes/console.php
+++ routes/console.php
@@ -1,19 +1,9 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-use App\Console\Commands\RateLimitWorker;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
-
-Artisan::starting(function ($artisan) {
-    $artisan->resolveCommands([
-        RateLimitWorker::class,
-    ]);
-});
-
-Artisan::command('rate-limit:process', function () {
-    $this->comment('Рейт-лимит IP-адресов');
-})->purpose('Рейт-лимит IP-адресов')->hourly();
