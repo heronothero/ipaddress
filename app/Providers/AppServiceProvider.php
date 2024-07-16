bca5e529a60f427deb8cf365a49987b4693a5cb9
heronothero <mazayadasha@gmail.com>
2024-07-16 22:14:03 +0700

    добавление в сервиса воркера для рейт-лимита

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,8 +1,12 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Console\Commands\RateLimitWorker;
+use App\Services\IPInfo\RateLimit;
+use App\Services\IPInfo\RateLimitContract;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +15,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(RateLimitContract::class, function ($app) {
+            return new RateLimit(40);
+        });
+        $this->commands([
+            RateLimitWorker::class,
+        ]);
     }
 
     /**
