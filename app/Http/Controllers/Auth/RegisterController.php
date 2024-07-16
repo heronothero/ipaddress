501eedf2be09b90ea89bafc461c6bf6c8c02e1ce
heronothero <mazayadasha@gmail.com>
2024-07-16 14:49:46 +0700

    заполнение контроллера регистрации

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,9 +4,33 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use App\Services\IPInfo\IPAPIService;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
-    
+    protected $IPAPIService;
+    public function __construct(IPAPIService $IPAPIService)
+    {
+        $this->IPAPIService = $IPAPIService;
+    }
+    public function showRegistrationForm(){
+        return view('registration');
+    }
+    public function register(Request $request){
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+            'ip' => $request->ip(),
+        ]);
+        $ipDetails = $this->IPAPIService->getIPInfo($request->ip());
+        return redirect('/')->with('success', 'Registration successful!');
+    }
 }
