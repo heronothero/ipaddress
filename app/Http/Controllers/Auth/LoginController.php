852b19a8a36df9288daa86840e6ce9522437298c
heronothero <mazayadasha@gmail.com>
2024-07-16 14:57:58 +0700

    заполнение контроллера авторизации

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -1,11 +1,27 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('authorization');
+    }
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/');
+        }
+        return back()->withErrors([
+            'email' => 'Неверный email или пароль.',
+        ]);
+    }
 }
