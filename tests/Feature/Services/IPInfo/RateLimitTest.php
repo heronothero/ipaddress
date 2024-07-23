364109dd386bdd4dd4768fd16a7d9c72bf31bb6d
heronothero <mazayadasha@gmail.com>
2024-07-24 04:22:31 +0700

    редактирование теста на рейт-лимиты

--- tests/Feature/Services/IPInfo/RateLimitTest.php
+++ tests/Feature/Services/IPInfo/RateLimitTest.php
@@ -10,25 +10,40 @@ use Illuminate\Support\Facades\Cache;
 
 class RateLimitTest extends TestCase
 {
+    /**
+     * Summary of setUp
+     */
     protected function setUp(): void
     {
         parent::setUp();
         Cache::flush();
     }
-    public function test_can_make_request_within_limit()
+
+    /**
+     * Test that requests can be made within the limit
+     */
+    public function test_can_make_request_within_limit(): void
     {
         $rateLimiter = new RateLimit();
         $this->assertTrue($rateLimiter->canMakeRequest());
         $rateLimiter->incrementRequestCount();
         $this->assertTrue($rateLimiter->canMakeRequest());
     }
-    public function test_cannot_make_request_after_limit_exceeded()
+
+    /**
+     * Test that requests cannot be made after the limit is exceeded
+     */
+    public function test_cannot_make_request_after_limit_exceeded(): void
     {
         $rateLimiter = new RateLimit();
         Cache::put(RateLimit::CACHE_KEY, 50);
         $this->assertFalse($rateLimiter->canMakeRequest());
     }
-    public function test_request_count_resets_after_decay_period()
+
+    /**
+     * Test that the request count resets after the decay period
+     */
+    public function test_request_count_resets_after_decay_period(): void
     {
         $rateLimiter = new RateLimit();
         Cache::put(RateLimit::CACHE_KEY, 40, 1);
